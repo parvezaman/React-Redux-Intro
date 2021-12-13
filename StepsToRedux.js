5fe1ff0782b0338f96e37bc563ae37672da4bf44
parvezaman <emailtoamanullah@gmail.com>
2021-12-13 12:58:56 +0600

    first up

---
+++ StepsToRedux.js
@@ -0,0 +1,26 @@
+/* 
+Key Terms / Terminologies
+--------------
+1.Reducer: The Banker
+2.Action: The initiative of function. What the servant does
+3.Action Creator: The function. The servant
+4.Action Type: Method. Type that servant writes in the letter
+5.Action Payload: ammount. The ammount servant writes in the envelop
+6.Dispatch: what the pegion does
+7.Store: The bank
+*/
+
+/* 
+Steps
+----------
+installaton
+------------
+1. npx create-react-app app-name
+2. npm install redux react-redux
+
+create store and reducer
+=========================
+1. create a folder in src named state/store whatever
+2. create another folder named reducers into it
+
+*/
